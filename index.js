b9311900068d661a084b6104f299402940047b03
goldyvilla2-pixel <goldyvilla2@gmail.com>
2026-03-15 18:54:44 +0530

    fix: set puppeteer executable path for Render

--- index.js
+++ index.js
@@ -43,6 +43,7 @@ const client = new Client({
     authStrategy: new LocalAuth({ dataPath: './session' }),
     puppeteer: {
         args: ['--no-sandbox', '--disable-setuid-sandbox'],
+        executablePath: '/usr/bin/google-chrome-stable',
         headless: true
     }
 });
